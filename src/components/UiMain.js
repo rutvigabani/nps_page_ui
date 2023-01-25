c41ef7c2686a6108c13ea6fb96faaf4405c833ec
rutvigabani <rutvigabani75@gmail.com>
2023-01-25 13:58:11 +0530

    initial commit

---
+++ src/components/UiMain.js
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export const UiMain = () => {
+  return (
+    <>
+      <div style={{ padding: "20px" }}>
+        <Typography variant="h6" fontSize="17px" fontWeight="600">
+          Customize NPS/Survey theme
+        </Typography>
+        <Typography variant="subtitle2" fontSize="13px" color="#9b9b96">
+          Customized theme to according your requirement.
+        </Typography>
+      </div>
+    </>
+  );
+};
